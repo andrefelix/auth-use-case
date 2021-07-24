8c9adb99115036af2a91f328209a949157a7e499
andre.felix <afelixfreitas@gmail.com>
2021-07-24 02:55:13 -0300

    refactor: cleared auth method

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -2,9 +2,12 @@ const { MissingParamError } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
   async auth (email, password) {
-    if (!email || !password) {
-      const missingParam = !email ? 'email' : 'password'
-      throw new MissingParamError(missingParam)
+    if (!email) {
+      throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
     }
   }
 }
