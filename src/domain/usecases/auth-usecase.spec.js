75822240a758a96e33c18fcd37dc4ec582e30c4e
andre.felix <afelixfreitas@gmail.com>
2021-07-29 03:22:55 -0300

    refactor: refactor makeSut helper method

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -2,18 +2,20 @@ const AuthUseCase = require('./auth-usecase')
 const { MissingParamError } = require('../../utils/errors')
 
 function makeSut () {
-  return new AuthUseCase()
+  const sut = new AuthUseCase()
+
+  return { sut }
 }
 
 describe('Auth Use Case', () => {
   it('should throw Error if no email is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const promise = sut.auth()
     await expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   it('should throw Error if no password is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const promise = sut.auth('any_email@mail.com')
     await expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
