0f7d2aecac8cbc6d591c1e7a2e1c19a9ae8b59e2
andre.felix <afelixfreitas@gmail.com>
2021-08-07 04:48:08 -0300

    feat: ensure that LoadUserByEmailRepository returns an user if user is found

--- src/infra/repositories/load-user-by-email.spec.js
+++ src/infra/repositories/load-user-by-email.spec.js
@@ -1,15 +1,40 @@
 const { MongoClient } = require('mongodb')
 
+const COLLECTION_USERS = 'users'
+
+let connection = null
+let db = null
+
 class LoadUserByEmailRepository {
-  async load () {
-    return null
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async load (email) {
+    const user = this.userModel.findOne({ email })
+    return user
   }
 }
 
-describe('LoadUserByEmail Repository', () => {
-  let connection
-  let db
+async function makeSut () {
+  const mockUser = {
+    _id: 'some_user_id',
+    email: 'valid_email@mail.com',
+    password: 'hashed_password',
+    age: 32,
+    state: 'any_state'
+  }
 
+  const userModel = db.collection(COLLECTION_USERS)
+
+  await userModel.insertOne(mockUser)
+
+  const sut = new LoadUserByEmailRepository(userModel)
+
+  return { sut, mockUser }
+}
+
+describe('LoadUserByEmail Repository', () => {
   beforeAll(async () => {
     connection = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
@@ -19,16 +44,22 @@ describe('LoadUserByEmail Repository', () => {
   })
 
   beforeEach(async () => {
-    await db.collection('COLLECTION_NAME').deleteMany({})
+    await db.collection(COLLECTION_USERS).deleteMany({})
   })
 
   afterAll(async () => {
     await connection.close()
   })
 
-  it('should retrn null if no user is found', async () => {
-    const sut = new LoadUserByEmailRepository()
+  it('should return null if no user is found', async () => {
+    const { sut } = await makeSut()
     const user = await sut.load('invalid_email@mail.com')
     expect(user).toBeNull()
+  })
+
+  it('should return an user if user is found', async () => {
+    const { sut, mockUser } = await makeSut()
+    const user = await sut.load(mockUser.email)
+    expect(user).toEqual(mockUser)
   })
 })
