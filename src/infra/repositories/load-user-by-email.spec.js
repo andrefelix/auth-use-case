1c2419758cf0d67becb31f83a621d857d2ae9a13
andre.felix <afelixfreitas@gmail.com>
2021-08-07 04:27:25 -0300

    chore: instal and config @shelf/jest-mongodb library

--- src/infra/repositories/load-user-by-email.spec.js
+++ src/infra/repositories/load-user-by-email.spec.js
@@ -1,3 +1,5 @@
+const { MongoClient } = require('mongodb')
+
 class LoadUserByEmailRepository {
   async load () {
     return null
@@ -5,6 +7,25 @@ class LoadUserByEmailRepository {
 }
 
 describe('LoadUserByEmail Repository', () => {
+  let connection
+  let db
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    db = await connection.db()
+  })
+
+  beforeEach(async () => {
+    await db.collection('COLLECTION_NAME').deleteMany({})
+  })
+
+  afterAll(async () => {
+    await connection.close()
+  })
+
   it('should retrn null if no user is found', async () => {
     const sut = new LoadUserByEmailRepository()
     const user = await sut.load('invalid_email@mail.com')
