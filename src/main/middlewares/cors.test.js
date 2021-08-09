1bafe50af91b7b1efb6819dc9726f76fdcf5e013
andre.felix <afelixfreitas@gmail.com>
2021-08-09 12:55:57 -0300

    refactor: move middleware test CORS to its own file

---
+++ src/main/middlewares/cors.test.js
@@ -0,0 +1,11 @@
+const app = require('../config/app')
+const request = require('supertest')
+
+it('should enable CORS', async () => {
+  app.get('/test_cors', (req, res) => { res.send('') })
+
+  const res = await request(app).get('/test_cors')
+  expect(res.headers['access-control-allow-origin']).toBe('*')
+  expect(res.headers['access-control-allow-methods']).toBe('*')
+  expect(res.headers['access-control-allow-headers']).toBe('*')
+})
