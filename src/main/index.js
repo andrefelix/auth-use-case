4199c82a34d51799bf4347a2714a338a1f47c44e
andre.felix <afelixfreitas@gmail.com>
2021-08-14 20:50:34 -0300

    feat: connect to mongodb before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,10 @@
-const app = require('./config/app')
+const MongodbHelper = require('../infra/helpers/mongodb-helper')
+const env = require('./config/env')
 
-app.listen(5858, () => console.log('Server running'))
+MongodbHelper
+  .connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app')
+    app.listen(5858, () => console.log('Server running'))
+  })
+  .catch(console.error)
