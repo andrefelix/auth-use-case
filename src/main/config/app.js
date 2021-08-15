adb4c947686628a2b51ed5f3c08b2f0465e1887c
andre.felix <afelixfreitas@gmail.com>
2021-08-14 21:43:42 -0300

    feat: add routes file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const setup = require('./setup')
+const routes = require('../config/routes')
 
 setup(app)
+routes(app)
 
 module.exports = app
