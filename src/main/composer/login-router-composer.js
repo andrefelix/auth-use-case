0bbf58727982e6641d675e4fdf0f6ce70405f906
andre.felix <afelixfreitas@gmail.com>
2021-08-17 09:17:35 -0300

    feat: created login route composition

---
+++ src/main/composer/login-router-composer.js
@@ -0,0 +1,20 @@
+const LoginRouter = require('../../presentation/routers/loging-router')
+const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
+const UpdateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository')
+const Encrypter = require('../../utils/encrypter')
+const TokenGenerator = require('../../utils/token-generator')
+const AuthUseCase = require('../../domain/usecases/auth-usecase')
+const EmailValidator = require('../../utils/email-validator')
+const env = require('../config/env')
+
+const authUseCase = new AuthUseCase({
+  loadUserByEmailRepository: new LoadUserByEmailRepository(),
+  updateAccessTokenRepository: new UpdateAccessTokenRepository(),
+  encrypter: new Encrypter(),
+  tokenGenerator: new TokenGenerator(env.tokenSecret)
+})
+
+const emailValidator = new EmailValidator()
+const loginRouter = new LoginRouter(authUseCase, emailValidator)
+
+module.exports = loginRouter
