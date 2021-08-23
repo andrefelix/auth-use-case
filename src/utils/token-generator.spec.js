6df93e0607c3b4261601165d81b382d20d244667
andre.felix <afelixfreitas@gmail.com>
2021-08-22 22:21:43 -0300

    fix: passing correct payload to jwt sign method

--- src/utils/token-generator.spec.js
+++ src/utils/token-generator.spec.js
@@ -3,8 +3,8 @@ jest.mock('jsonwebtoken', () => ({
   id: '',
   secret: '',
 
-  sign (id, secret) {
-    this.id = id
+  sign (payload, secret) {
+    this.payload = payload
     this.secret = secret
     return this.token
   }
@@ -41,7 +41,7 @@ describe('Token Generator', () => {
   it('should call JWT with correct values', async () => {
     const sut = makeSut()
     await sut.generate('any_id')
-    expect(jwt.id).toBe('any_id')
+    expect(jwt.payload).toEqual({ _id: 'any_id' })
     expect(jwt.secret).toBe(sut.secret)
   })
 
