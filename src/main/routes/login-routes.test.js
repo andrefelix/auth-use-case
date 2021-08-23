dedc76cb3a0b23dfaa75e911b4b1673c09c1c57d
andre.felix <afelixfreitas@gmail.com>
2021-08-22 22:26:58 -0300

    feat: ensure that LoginRoutes return statusCode 401 when invalid credentials are provided

--- src/main/routes/login-routes.test.js
+++ src/main/routes/login-routes.test.js
@@ -44,4 +44,14 @@ describe('LoginRoutes', () => {
       })
       .expect(200)
   })
+
+  it('should return 401 when invalid credentials are provided', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: fakeUser.email,
+        password: fakeUser.password
+      })
+      .expect(401)
+  })
 })
